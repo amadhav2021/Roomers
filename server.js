8e87f8565f66fb34c956cb1fb3498356c8c80447
amadhav2021 <amadhav2021@gmail.com>
2020-08-18 14:45:01 -0400

    root endpoint added

--- server.js
+++ server.js
@@ -4,6 +4,10 @@ const server = require('http').Server(app)
 const io = require('socket.io')(server)
 const PORT = 3000 || process.env.PORT
 
+app.get('/', (req, res) => {
+    res.send("Hello World!")
+})
+
 server.listen(PORT, () => {
     console.log('Server started on port: ', PORT)
 })
