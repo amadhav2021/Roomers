3d58351d04ad738441b61fef8dfece7a4768396a
amadhav2021 <amadhav2021@gmail.com>
2020-08-18 15:27:24 -0400

    basic server

--- server.js
+++ server.js
@@ -1,13 +1,19 @@
-const express = require('express')
-const app = express()
-const server = require('http').Server(app)
-const io = require('socket.io')(server)
-const PORT = 3000 || process.env.PORT
+#!/usr/bin/nodejs
 
-app.get('/', (req, res) => {
-    res.send("Hello World!")
-})
+// -------------- load packages -------------- //
+var express = require('express');
+var app = express();
+var path = require('path');
 
-server.listen(PORT, () => {
-    console.log('Server started on port: ', PORT)
-})
+// -------------- express initialization -------------- //
+app.set('port', process.env.PORT || 3000);
+
+// -------------- express endpoints -------------- //
+app.get('/', function(req, res){
+    res.send('peep');
+});
+
+// -------------- listener -------------- //
+var listener = app.listen(app.get('port'), function(){
+    console.log('Express server started on port: ' + listener.address().port);
+});
