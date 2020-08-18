e7cc2fba35292350c30d8ef93348f938de7fba86
amadhav2021 <amadhav2021@gmail.com>
2020-08-18 16:12:02 -0400

    socket.io server initialized

--- server.js
+++ server.js
@@ -1,13 +1,25 @@
+// Packages and server setup
 const express = require('express')
 const app = express()
 const PORT = process.env.PORT || 3000
 const server = require('http').createServer(app)
 const io = require('socket.io').listen(server)
+const path = require('path')
 
+// Set static folder
+app.use(express.static(path.join(__dirname, 'public')))
+
+// Run when new connection occurs
+io.on('connection', socket => {
+    console.log('New socket connection...')
+})
+
+// Endpoints
 app.get('/', (req, res) => {
     res.send('hello')
 })
 
+// Set server to listen to port
 server.listen(PORT, () => {
     console.log('Server running on port ', PORT)
 })
