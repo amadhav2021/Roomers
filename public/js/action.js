c64eee73f3b4616d679dbdbfafacc483f1139de8
amadhav2021 <amadhav2021@gmail.com>
2020-08-18 23:20:52 -0400

    enter key to send message

--- public/js/action.js
+++ public/js/action.js
@@ -2,6 +2,14 @@ document.querySelector('h1').addEventListener('click', () => {
     alert('Thanks for clicking!')
 })
 
+document.querySelector('#msg').onkeydown = (e) => {
+    if(e.keyCode == 13){
+        val = document.querySelector('#msg').value
+        document.querySelector('#msg').value = ''
+        alert(val)
+    }
+}
+
 const socket = io()
 
 socket.on('welcome', data => {
