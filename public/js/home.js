f67475ae3043caada161dcb51b4caf05d3897460
amadhav2021 <amadhav2021@gmail.com>
2020-09-06 18:59:35 -0400

    changed char limit for heroku

--- public/js/home.js
+++ public/js/home.js
@@ -4,7 +4,7 @@ document.querySelector("#roomInput").focus()
 // Validate form before it is submitted
 document.querySelector("#roomForm").addEventListener("submit", function(e){
     room = document.querySelector("#roomInput").value.trim()
-    if(room === '' || room.length > 8){
+    if(room === '' || room.length > 5){
         e.preventDefault(); //stop form from submitting
         document.querySelector("#roomInput").value = ''
         document.querySelector("#roomInput").focus()
