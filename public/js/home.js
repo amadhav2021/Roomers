b96e544e567135de25fe3b9c9bee797d3f47bc2d
amadhav2021 <amadhav2021@gmail.com>
2020-09-06 18:56:02 -0400

    validated room name form

---
+++ public/js/home.js
@@ -0,0 +1,15 @@
+// Focus on the name so user doesn't have to click
+document.querySelector("#roomInput").focus()
+
+// Validate form before it is submitted
+document.querySelector("#roomForm").addEventListener("submit", function(e){
+    room = document.querySelector("#roomInput").value.trim()
+    if(room === '' || room.length > 8){
+        e.preventDefault(); //stop form from submitting
+        document.querySelector("#roomInput").value = ''
+        document.querySelector("#roomInput").focus()
+    }
+    else{
+        document.querySelector("#roomInput").value = room
+    }
+});
